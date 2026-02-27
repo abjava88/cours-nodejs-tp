623816e7834d658eed434fb5bcc5bcbb6fc8d93a
aklaptop <aetklaptops@gmail.com>
2026-02-27 18:51:26 -0500

    Supprimer derniere partie.

--- ch02/exercise.js
+++ ch02/exercise.js
@@ -45,23 +45,3 @@ async function utiliserAdditionner() {
 }
 
 utiliserAdditionner();
-
-console.log("Fin du programme");
-
-// Exemple fetch asynchrone
-
-async function fetchData() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error("There was a problem with the fetch operation:", error);
-  }
-}
-
-fetchData();
-console.log("Find du programme");
