e19184c545c34724706a88ad7ae96fe77fc397b1
aklaptop <aetklaptops@gmail.com>
2026-02-28 08:50:59 -0500

    Ajouter exemple ch 03

---
+++ ch03/test.js
@@ -0,0 +1,6 @@
+const calculator = require("./calculator");
+
+console.log(`addition : ${calculator.add(10,20)}`);
+console.log(`soustraction : ${calculator.sub(10,20)}`);
+console.log(`Multiplication : ${calculator.mult(10,20)}`);
+console.log(`Division : ${calculator.div(10,20)}`);
